2b3ea6265567f9497fefe6e731876edaa19c835c
roteErde <not_so_good@outlook.com>
2018-02-20 18:44:24 +0800

    added telegram sample code

--- src/main.go
+++ src/main.go
@@ -1,6 +1,35 @@
 package main
 
-import "fmt"
-func main(){
-	fmt.Print("hello world")
+import (
+	"log"
+	"os"
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("KEY"))
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
